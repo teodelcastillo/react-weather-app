9773a2e7860def6a5ac0d26ef4e2ee1e69f0585c
Teodoro del Castillo <teodorodelcastillop@gmail.com>
2023-08-09 18:40:24 -0300

    Responsive settings

--- weather-app/src/components/WeatherInfo.jsx
+++ weather-app/src/components/WeatherInfo.jsx
@@ -5,7 +5,8 @@ function WeatherInfo({ temperature, weatherMain, tempMin, tempMax }) {
     <div className="current">
       <div className="temperature">{`${Math.round(temperature)}°C`}</div>
       <div className="weather">{weatherMain}</div>
-      <div className="min-max">{`${tempMin}°C / ${tempMax}°C`}</div>
+      <div className="min-max">
+        <p id='tempMin'>{tempMin}°C</p><p id='slash'>/</p><p id='tempMax'> {tempMax}°C</p></div>
     </div>
   );
 }
